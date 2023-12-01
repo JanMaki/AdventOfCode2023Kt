4d253698ee2e888a2b69012edefac3e65bc7035c
JanMaki <mutsukimaki@gmail.com>
2023-12-02 04:27:36 +0900

    Day01-part1を提出

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,47 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.sumOf {
+            //左の数値
+            val firstDigit: Int
+            //0から始める
+            var index = 0
+            while (true) {
+                //1文字抽出
+                val value = it.substring(index, index + 1).toIntOrNull()
+                if (value != null) {
+                    firstDigit = value
+                    break
+                }
+
+                //indexを進める
+                index++
+            }
+
+            //右の数値
+            val lastDigit: Int
+            //後ろから始める
+            var indexFromLast = it.length - 1
+            while (true) {
+                //1文字抽出
+                val value = it.substring(indexFromLast, indexFromLast + 1).toIntOrNull()
+                if (value != null) {
+                    lastDigit = value
+                    break
+                }
+
+                //indexを後退
+                indexFromLast--
+            }
+
+            //2桁の数字にする
+            firstDigit * 10 + lastDigit
+        }
     }
 
     fun part2(input: List<String>): Int {
         return input.size
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
     val input = readInput("Day01")
     part1(input).println()
-    part2(input).println()
 }
