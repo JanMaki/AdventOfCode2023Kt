5151f6a6953f8a8f7b9aaa1c54e357e088651c78
JanMaki <mutsukimaki@gmail.com>
2023-12-02 04:43:13 +0900

    Day01-part1の実装をいい感じに変更

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,40 +1,14 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.sumOf {
-            //左の数値
-            val firstDigit: Int
-            //0から始める
-            var index = 0
-            while (true) {
-                //1文字抽出
-                val value = it.substring(index, index + 1).toIntOrNull()
-                if (value != null) {
-                    firstDigit = value
-                    break
-                }
-
-                //indexを進める
-                index++
-            }
-
-            //右の数値
-            val lastDigit: Int
-            //後ろから始める
-            var indexFromLast = it.length - 1
-            while (true) {
-                //1文字抽出
-                val value = it.substring(indexFromLast, indexFromLast + 1).toIntOrNull()
-                if (value != null) {
-                    lastDigit = value
-                    break
-                }
-
-                //indexを後退
-                indexFromLast--
-            }
-
-            //2桁の数字にする
-            firstDigit * 10 + lastDigit
+        return input.sumOf { element ->
+            //charの配列に
+            element.toCharArray()
+                //charをIntかnullに
+                .map { it.digitToIntOrNull() }
+                //nullを排除
+                .filterNotNull()
+                //最初と最後の要素で値を作成
+                .let { it.first() * 10 + it.last() }
         }
     }
 
